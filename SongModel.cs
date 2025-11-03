b9005fcaad73c2bbce7709c966afb0b017440816
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2025-11-03 08:34:57 +0100

    Add SongModel class for song data representation

---
+++ SongModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MauiApp2
+{
+    public class SongModel
+    {
+        public string Title { get; set; }
+        public string Path { get; set; }
+    }
+}
